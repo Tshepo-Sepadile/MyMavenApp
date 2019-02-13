dfe9313795df392a932bd4d1c369fe012129f642
RasputinZA <47171762+RasputinZA@users.noreply.github.com>
2019-02-13 09:20:50 +0200

    Add files via upload

---
+++ src/main/java/MyApp/App.java
@@ -0,0 +1,14 @@
+package MyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi, how are you?" );
+        Person x = new Person();
+    }
+}
---
+++ src/main/java/MyApp/Person.java
@@ -0,0 +1,8 @@
+package MyApp;
+
+public class Person {
+    public Person()
+    {
+        System.out.println("I am well, thank you!");
+    }
+}
