fedcf740d70f88ccb528294c1ccb24c8578ed0b2
Tcheslavie <valeryburtsev@yahoo.com>
2019-02-13 09:29:43 +0200

    Update App.java

--- src/main/java/MyApp/App.java
+++ src/main/java/MyApp/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi, how are you?" );
+        System.out.println( "Hi, how are you buddy?" );
         Person x = new Person();
     }
 }
